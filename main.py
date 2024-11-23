9c3ceb84336c9662d67636cce47ad8cecd7e8533
AIMAD KACEM <kacem.aimad@gmail.com>
2024-11-23 20:07:34 +0100

    finish 2 rework station option

--- main.py
+++ main.py
@@ -34,8 +34,38 @@ if option == '1':
                        rework_card,
                        fault,
                        start_time,
-                       "",""])
+                       Null,
+                       Null])
     exit(0)
 elif option == '2':
     step = 'end_rework'
+    filename = get_csv_name(step)
+    print(filename)
+    qr_code = input('Scan Rework Label Qr Code\n')
+    reword_data = qr_code.split(';')
+    ref_data = check_ref(reword_data[1])
+    if ref_data is None:
+        print('Reference Not Exist on Database!!!!')
+        exit(-1)
     
+    rework_card = reword_data[0]
+    path = 'data/data_rework/{}'.format(filename)
+    data = read_from_csv(path, rework_card)
+    if data:
+        print('harness already exist')
+        exit(-1)
+    fault = input('enter Fault Description\n')
+    start_time = datetime.strptime(reword_data[2], "%Y-%m-%d %H:%M:%S")
+    end_time = datetime.now()
+    rework_time = end_time - start_time
+    rework_time = rework_time.total_seconds() / 60
+    save_to_csv(path,
+                [operator,
+                 ref_data[1],
+                 ref_data[0],
+                 rework_card,
+                 fault,
+                 start_time.strftime("%Y-%m-%d %H:%M:%S"),
+                 end_time.strftime("%Y-%m-%d %H:%M:%S"),
+                 round(rework_time, 2)])
+    exit(0)
